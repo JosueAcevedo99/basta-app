809ab16a2dacdedee69f71495c1860416eeefa11
Josué Acevedo <105468315+JosueAcevedo99@users.noreply.github.com>
2022-10-05 14:08:27 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+let categorias = ["CIUDAD","OBJETO ESCOLAR","COLOR","SABOR","FLOR FRUTO VEGETAL", "ANIMAL", "MARCA", "COMIDA", "CELEBRIDAD", "PELICULA", 
+                  "CANCION", "PRENDA", ];
+
+const letraActual = document.getElementById('letra');
+
+var letranueva = letras[Math.floor(Math.random()*letras.length)];
+
+
+var intervalLetra = setInterval(ChangerLetras, 5000, letraActual);
+
+function ChangerLetras() {
+    letraActual.innerHTML = letras[Math.floor(Math.random()*letras.length)];
+}
+
+const categoriaActual = document.getElementById('categoria');
+
+var categorianueva = categorias[Math.floor(Math.random()*categorias.length)];
+
+var intervalcategoria = setInterval(ChangerCategoria, 5000, categoriaActual);
+
+function ChangerCategoria() {
+  categoriaActual.innerHTML = categorias[Math.floor(Math.random()*categorias.length)];
+}
+
+
+
