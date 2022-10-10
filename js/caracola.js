6cb28853a996f93f027cc52527adacc88f0b8e6d
Josué Acevedo <105468315+JosueAcevedo99@users.noreply.github.com>
2022-10-10 12:57:55 -0500

    commit 37

--- js/caracola.js
+++ js/caracola.js
@@ -1,9 +1,9 @@
 let categorias = ["NOP", "SIP", "IZQUIERDA DECIDE", "DERECHA DECIDE", 
                   "ULTIMO GANADOR DECIDE", "ULTIMO LUGAR ACTUAL DECIDE",
-                  "ULTIMO PERDEDPR DECIDE", "PROXIMO CUMPLEAÑERO",
+                  "ULTIMO PERDEDOR DECIDE", "PROXIMO CUMPLEAÑERO",
                   "MAS JOVEN", "MAS VIEJO", "EDADES PARES", "EDADES NONES",
                   "HOMBRES", "MUJERES", "PLAYERA CLARA", "PLAYERA OSCURA",
-                  "ABRIGO CLARO", "ABRIGO OSCURO", 
+                  "ABRIGO CLARO", "ABRIGO OSCURO", "DE NUEVO"
 ];
 
 
@@ -19,36 +19,36 @@ const btnWrong4 = document.getElementById('wrong4');
 
 
 btnCorrect.onclick = () =>{
-  alert("1.- " +  categorias[Math.floor(Math.random()*categorias.length)] + "\n 2.- " + categorias[Math.floor(Math.random()*categorias.length)]);
+  alert(categorias[Math.floor(Math.random()*categorias.length)]);
   location.reload();
 }
 
 btnWrong.onclick = () =>{
-  alert("1.- " +  categorias[Math.floor(Math.random()*categorias.length)] + "\n 2.- " + categorias[Math.floor(Math.random()*categorias.length)]);
+  alert(categorias[Math.floor(Math.random()*categorias.length)]);
   location.reload();
 }
 btnCorrect2.onclick = () =>{
-  alert("1.- " +  categorias[Math.floor(Math.random()*categorias.length)] + "\n 2.- " + categorias[Math.floor(Math.random()*categorias.length)]);
+  alert(categorias[Math.floor(Math.random()*categorias.length)]);
   location.reload();  
 }
 btnWrong2.onclick = () =>{
-  alert("1.- " +  categorias[Math.floor(Math.random()*categorias.length)] + "\n 2.- " + categorias[Math.floor(Math.random()*categorias.length)]);
+  alert(categorias[Math.floor(Math.random()*categorias.length)]);
   location.reload();
 }
 btnCorrect3.onclick = () =>{
-  alert("1.- " +  categorias[Math.floor(Math.random()*categorias.length)] + "\n 2.- " + categorias[Math.floor(Math.random()*categorias.length)]);
+  alert(categorias[Math.floor(Math.random()*categorias.length)]);
   location.reload();  
 }
 btnWrong3.onclick = () =>{
-  alert("1.- " +  categorias[Math.floor(Math.random()*categorias.length)] + "\n 2.- " + categorias[Math.floor(Math.random()*categorias.length)]);
+  alert(categorias[Math.floor(Math.random()*categorias.length)]);
   location.reload();
 }
 btnCorrect4.onclick = () =>{
-  alert("1.- " +  categorias[Math.floor(Math.random()*categorias.length)] + "\n 2.- " + categorias[Math.floor(Math.random()*categorias.length)]);
+  alert(categorias[Math.floor(Math.random()*categorias.length)]);
   location.reload();  
 }
 btnWrong4.onclick = () =>{
-  alert("1.- " +  categorias[Math.floor(Math.random()*categorias.length)] + "\n 2.- " + categorias[Math.floor(Math.random()*categorias.length)]);
+  alert(categorias[Math.floor(Math.random()*categorias.length)]);
   location.reload();
 }
 
