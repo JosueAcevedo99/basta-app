a607731dde22a0c5d2996f1e5c613dfe78d6e136
Josué Acevedo <105468315+JosueAcevedo99@users.noreply.github.com>
2022-10-12 16:47:19 -0500

    commit 63

--- js/memory.js
+++ js/memory.js
@@ -1,27 +1,6 @@
 $(function(){
   
-    function set(key, value) { localStorage.setItem(key, value); }
-    function get(key)        { return localStorage.getItem(key); }
-    function increase(el)    { set(el, parseInt( get(el) ) + 1); }
-    function decrease(el)    { set(el, parseInt( get(el) ) - 1); }
-  
-    var toTime = function(nr){
-      if(nr == '-:-') return nr;
-      else { var n = ' '+nr/1000+' '; return n.substr(0, n.length-1)+'s'; }
-    };
-  
-    function updateStats(){
-      $('#stats').html('<div class="padded"><h2>Figures: <span>'+
-        '<b>'+get('flip_won')+'</b><i>Won</i>'+
-        '<b>'+get('flip_lost')+'</b><i>Lost</i>'+
-        '<b>'+get('flip_abandoned')+'</b><i>Abandoned</i></span></h2>'+
-        '<ul><li><b>Best Casual:</b> <span>'+toTime( get('flip_casual') )+'</span></li>'+
-        '<li><b>Best Medium:</b> <span>'+toTime( get('flip_medium') )+'</span></li>'+
-        '<li><b>Best Hard:</b> <span>'+toTime( get('flip_hard') )+'</span></li></ul>'+
-        '<ul><li><b>Total Flips:</b> <span>'+parseInt( ( parseInt(get('flip_matched')) + parseInt(get('flip_wrong')) ) * 2)+'</span></li>'+
-        '<li><b>Matched Flips:</b> <span>'+get('flip_matched')+'</span></li>'+
-        '<li><b>Wrong Flips:</b> <span>'+get('flip_wrong')+'</span></li></ul></div>');
-    };
+   
   
     function shuffle(array) {
       var currentIndex = array.length, temporaryValue, randomIndex;
@@ -44,41 +23,11 @@ $(function(){
       $('.c3').text(text.substring(2, 3));
       $('.c4').text(text.substring(3, 4));
   
-      // If won game
-      if(text == 'nice'){
-        increase('flip_won');
-        decrease('flip_abandoned');
-      }
   
-      // If lost game
-      else if(text == 'fail'){
-        increase('flip_lost');
-        decrease('flip_abandoned');
-      }
-  
-      // Update stats
-      updateStats();
     };
   
-    /* LOAD GAME ACTIONS */
-  
-    // Init localStorage
-    if( !get('flip_won') && !get('flip_lost') && !get('flip_abandoned') ){
-      //Overall Game stats
-      set('flip_won', 0);
-      set('flip_lost', 0);
-      set('flip_abandoned', 0);
-      //Best times
-      set('flip_casual', '-:-');
-      set('flip_medium', '-:-');
-      set('flip_hard', '-:-');
-      //Cards stats
-      set('flip_matched', 0);
-      set('flip_wrong', 0);
-    }
-  
-    // Fill stats
-    if( get('flip_won') > 0 || get('flip_lost') > 0 || get('flip_abandoned') > 0) {updateStats();}
+
+
   
     // Toggle start screen cards
     $('.logo .card:not(".twist")').on('click', function(e){
@@ -88,7 +37,7 @@ $(function(){
   
     // Start game
     $('.play').on('click', function(){
-      increase('flip_abandoned');
+     
           $('.info').fadeOut();
   
       var difficulty = '',
@@ -134,64 +83,22 @@ $(function(){
   
                 if( thisCard.length > 1 ) {
                   thisCard.parents('.card').toggleClass('active card found').empty(); //yey
-                  increase('flip_matched');
   
                   // Win game
                   if( !$('#g .card').length ){
-                    var time = $.now() - startGame;
-                    if( get('flip_'+difficulty) == '-:-' || get('flip_'+difficulty) > time ){
-                      set('flip_'+difficulty, time); // increase best score
-                    }
-  
-                    startScreen('nice');
+                    
+                    setTimeout(function() {if(!alert('Se terminó')){window.location.reload();}},500);
                   }
                 }
                 else {
                   $('#g .card.active').removeClass('active'); // fail
-                  increase('flip_wrong');
                 }
               }, 401);
             }
           }
         });
   
-        // Add timer bar
-        $('<i class="timer"></i>')
-          .prependTo('#g')
-          .css({
-            //'animation' : 'timer '+timer+'ms linear'
-          })
-          .one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-            startScreen('fail'); // fail game
-          });
-  
-        // Set keyboard (p)ause and [esc] actions
-        $(window).off().on('keyup', function(e){
-          // Pause game. (p)
-          if(e.keyCode == 80){
-            if( $('#g').attr('data-paused') == 1 ) { //was paused, now resume
-              $('#g').attr('data-paused', '0');
-              $('.timer').css('animation-play-state', 'running');
-              $('.pause').remove();
-            }
-            else {
-              $('#g').attr('data-paused', '1');
-              $('.timer').css('animation-play-state', 'paused');
-              $('<div class="pause"></div>').appendTo('body');
-            }
-          }
-          // Abandon game. (ESC)
-          if(e.keyCode == 27){
-            startScreen('flip');
-            // If game was paused
-            if( $('#g').attr('data-paused') == 1 ){
-              $('#g').attr('data-paused', '0');
-              $('.pause').remove();
-            }
-            $(window).off();
-          }
         });
       });
     });
     
-  });
